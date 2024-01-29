c825e6c86f0c598e55bc3743fe69dd1241d8c458
classic-k <88990430+classic-k@users.noreply.github.com>
2024-01-29 00:33:23 -0800

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -11,10 +11,14 @@ import json
 
 load_dotenv()
 
-API_ID  = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-PHONE_NUM = os.getenv("PHONE_NUM")
+
+API_ID  = os.getenv("API_ID")#27830331 #1307728
+API_HASH = os.getenv("API_HASH") #"4e7e94ade6b47dca634affc4478c3f63" #'0fdacb025cf6bfa8585007b565920454'
+PHONE_NUM = os.getenv("PHONE_NUM") #'+2347033637812'
 SLEEP_TIME = os.getenv("SLEEP_TIME")
+CHAT_LINK ="https://t.me/solana" #"https://t.me/DUROMEDIA2"
+New_Channel = "https://t.me/baleers"
+
 
 async def connect():
 
@@ -22,15 +26,15 @@ async def connect():
 
     client = TelegramClient("anon", API_ID, API_HASH)
 
-    if await client.start() and await client.is_user_authorized():
+    
+
+  
+    
+    if not await client.start(): #and await client.is_user_authorized():
 
         #tgClient = client
-        print("Client is ready to shoot")
-        return client
-    
-    if not client.start():
-
-        print("Client not auhtenticated")
+        print("Client is not ready to shoot, try again")
+        return #client
 
     else:
 
@@ -54,11 +58,10 @@ async def connect():
                     current_user = client.sign_in(password=pw)
                     print("Client activated")
                     #print("An error occur")
+    
 
     
     return client
-
-
 
 
 async def add(tgClient,users, mychannel):
@@ -66,27 +69,27 @@ async def add(tgClient,users, mychannel):
     await tgClient.start()
     channel_entity = await tgClient.get_entity(mychannel)
     usernames = []
-   
+    count = 0
     for user in users:
-        print(user)
-        username = user["username"]
         
-        #is_member = await isMember(tgClient, username, mychannel)
-        #if is_member:
-        #continue
+        username = user["username"]
         usernames.append(username)
-    try:
-        await tgClient(InviteToChannelRequest(channel_entity, usernames))
-    except Exception as err:
-        print(err)
+        count = count + 1
+        if count % 4 == 0:
+            sl = usernames[count-4:count]
+            try:
+                await tgClient(InviteToChannelRequest(channel_entity, sl))
+                print("sleeping now after adding")
+                time.sleep(15)
+            except Exception as err:
+                print(err)
            
-
 
 async def getChannelMembers(tgClient, channel):
     users = []
     result = []
     entity = await tgClient.get_entity(channel)
-    offset = 0
+    offset = getOffset()
     limit = 0
     while True:
         members = await tgClient(GetParticipantsRequest(channel=entity,filter=ChannelParticipantsSearch(''), offset=offset, limit=limit, hash=0))
@@ -125,7 +128,17 @@ def writeToFile(lists, filename="users.json"):
     print("Content after:", len(content))
     with open(filename, "w") as f:
         json.dump(content, f)
+def getOffset():
+    l = 0
+    try:
 
+        with open("users.json", "r") as f:
+            contents = json.load(f)
+            l = len(list(contents))
+    except Exception as err:
+        
+        l = 0
+    return l
 async def isMember(tgClient,channel, username):
     limit = 100
     offset = 0
